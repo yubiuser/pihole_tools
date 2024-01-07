9d2a8e2ba85f245a6de854888779f1bfcc7c9ace
Christian König <ckoenig@posteo.de>
2024-01-07 21:09:40 +0100

    Return code 200 is gone
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- pihole_FTL_API.sh
+++ pihole_FTL_API.sh
@@ -178,7 +178,6 @@ DeleteSession() {
         deleteResponse=$(curl -skS -o /dev/null -w "%{http_code}" -X DELETE "${API_URL}auth"  -H "Accept: application/json" -H "sid: ${SID}")
 
         case "${deleteResponse}" in
-            "200") printf "%b" "A session that was not created cannot be deleted (e.g., empty API password).\n";;
             "204") printf "%b" "Session successfully deleted.\n";;
             "401") printf "%b" "Logout attempt without a valid session. Unauthorized!\n";;
          esac;
